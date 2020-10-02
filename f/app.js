c3e01673734a5a3449fe137be95c45de24cd6f18
BrandonMejia <brandonprogamer02@gmail.com>
2020-10-01 22:25:15 -0400

    se actualizo en javascript/f

--- f/app.js
+++ f/app.js
@@ -1,55 +1,59 @@
-let request;
+
 //---EVENTOS LISTENERS-------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', () => {
-    request = indexedDB.open('db', 1);
     loadIndexedBD();
-    readData();
+    let f = readData();
+    //console.log(f);
     addData();
 });
 
 // --REAL FUNCTIONS-----------------------------------------------------------------------
 const loadIndexedBD = () => {
     // esto se ejecuta cuando se crea por primera vez la db
+    let request = indexedDB.open('db', 1);
     request.onupgradeneeded = (evt) => {
         let DB = request.result;
         DB.createObjectStore('db', { autoIncrement: true });
         console.log('Almacen creado', DB);
     }
-    request.onsuccess = () => { }
     //si hay error
     request.onerror = (error) => console.log('hubo un error', error);
 
 }
 
-const addData = async() => {
-    let request =   indexedDB.open('db', 1);
-    request.onsuccess =  () => {
-        let DB =  request.result;
-        const transaccion =  DB.transaction(['db'], 'readwrite');
+const addData = async () => {
+    implementarCodigoIndexedDB('db', 'readwrite', (transaccion) => {
         const objectStorage = transaccion.objectStore('db');
         const newClient = {
             nombre: 'San Juan',
             email: 'sanjuan0100@gmail.com'
         }
-        const peticion = objectStorage.add(newClient);
-        console.log('Se realizo la peticion',  peticion);
-    }
+        let array = new Array();
+        array.push(newClient);
+        objectStorage.add(array);
+    });
 }
 
 const readData = () => {
-        let request = indexedDB.open('db', 1);
-        request.onsuccess = () => {
-            let DB = request.result;
-            const transaccion = DB.transaction(['db'], 'readonly');
-            const objectStorage = transaccion.objectStore('db');
-            const request2 = objectStorage.openCursor();
-
-            request2.onsuccess = () => {
-                const cursor = request2.result;
-                if(cursor)  // real sintaxis js
-                {   console.log(cursor.value);
-                    return cursor.continue()
-                } else{ console.log('no hay mas registros') } 
-            }
+    implementarCodigoIndexedDB('db', 'readonly', (transaccion) => {
+        const objectStorage = transaccion.objectStore('db');
+        const requestCursor = objectStorage.openCursor();
+        requestCursor.onsuccess = () => {
+            const cursor = requestCursor.result;
+            console.log('lectura de la database:');
+            if (cursor)  // real sintaxis js
+            {
+                console.log(cursor.continue());
+            } else { console.log('no hay mas registros') }
+            
+        }
+    });
+}
+const implementarCodigoIndexedDB = (db, tipoTransaccion, callback) => {
+    let request = indexedDB.open(db, 1);
+    request.onsuccess = () => {
+        let DB = request.result;
+        const transaccion = DB.transaction([db], tipoTransaccion);
+        callback(transaccion);
     }
 }
