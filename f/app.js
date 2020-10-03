9b5c52dc990544e61df64c69375021128c532104
Brandon Fernandez Mejia <=>
2020-10-02 23:11:50 -0400

    f/app.js => agregue la clase indexedDB y organize todo y meti metodos para crear y borrar tablas

--- f/app.js
+++ f/app.js
@@ -1,64 +1,115 @@
 
 //---EVENTOS LISTENERS-------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', () => {
-    loadIndexedBD();
-    // esta es la real
-    readData((arrayData)=>
-    {
-        console.log(arrayData);
-    });
-    addData( {nombre:'jose',apellido:"Urbaez Sanchez"} );
-    
+    let f = new IndexedDB();
+
+
 });
 
-// --REAL FUNCTIONS-----------------------------------------------------------------------
-const loadIndexedBD = () => {
-    // esto se ejecuta cuando se crea por primera vez la db
-    let request = indexedDB.open('db', 1);
-    request.onupgradeneeded = (evt) => {
-        let DB = request.result;
-        DB.createObjectStore('db', { autoIncrement: true });
-        console.log('Almacen creado', DB);
-    }
-    //si hay error
-    request.onerror = (error) => console.log('hubo un error', error);
 
-}
-const updateData = () => {
+class IndexedDB {
 
-}
-
-const addData = (objeto) => {
-    implementarCodigoIndexedDB('db', 'readwrite', (objectStorage) => {
-        objectStorage.add(objeto);
-    });
-}
-
-const readData = (callback) => {
-    implementarCodigoIndexedDB('db', 'readonly', (objectStorage) => {
-        const requestCursor = objectStorage.openCursor();
-        const arrayData = new Array();
-        requestCursor.onsuccess = (e) => {
-            const cursor = requestCursor.result;
-            if (cursor) {
-                arrayData.push(cursor.value);
-                cursor.continue();
-            } 
+    // --REAL FUNCTIONS-----------------------------------------------------------------------
+    constructor(db) {
+        // this it execute first
+        let request = indexedDB.open(db, 1);
+        request.onupgradeneeded = () => {
+            let DB = request.result;
+            DB.createObjectStore('db', { autoIncrement: true });
+            console.log('Almacen creado', DB);
         }
-        callback(arrayData);
-    });
+        // if a error
+        request.onerror = (error) => console.error('Was a error', error);
 
-}
-const implementarCodigoIndexedDB = (db, tipoTransaccion, callback) => {
-    let request = indexedDB.open(db, 1);
-
-    request.onsuccess = () => {
-        let DB = request.result;
-        const transaccion = DB.transaction([db], tipoTransaccion);
-        const objectStorage = transaccion.objectStore('db');
-        callback(objectStorage);
     }
+    deleteTable = (db,table) => {
+        let request = indexedDB.open(db, 1);
+        request.onupgradeneeded = () => {
+            let DB = request.result;
+            DB.deleteObjectStore(table);
+            console.log(`THE TABLE ${table} OF THE DB ${db} WAS DELETED`);
+        }
+
+    }
+    createTable = (db,table) => {
+        let request = indexedDB.open(db, 1);
+        request.onupgradeneeded = () => {
+            let DB = request.result;
+            DB.createObjectStore(table, { autoIncrement: true });
+            console.log(`THE TABLE ${table} WAS CREATED IN DB ${db}`);
+        }
+    }
+
+    deleteData = (db, table, key) => {
+        FimplementationCodeIndexedDB(db, table, (objectStorage) => {
+            const requestCursor = objectStorage.openCursor();
+            requestCursor.onsuccess = (e) => {
+                const cursor = requestCursor.result;
+                if (cursor) {
+                    if (cursor.key == key) {
+                        cursor.delete();
+                        console.log('se ha cambiado el valor');
+                        return;
+                    }
+                    cursor.continue();
+                }
+            }
+        });
+    }
+
+
+
+    updateData = (db, table, key, value) => {
+        implementationCodeIndexedDB(db, table, 'readwrite', (objectStorage) => {
+            const requestCursor = objectStorage.openCursor();
+            requestCursor.onsuccess = (e) => {
+                const cursor = requestCursor.result;
+                if (cursor) {
+                    if (cursor.key == key) {
+                        cursor.update(value);
+                        return;
+                    }
+
+                    cursor.continue();
+                }
+            }
+        });
+    }
+
+    addDataTable = (db, table, objeto) => {
+        implementationCodeIndexedDB(db, table, 'readwrite', (objectStorage) => {
+            objectStorage.add(objeto);
+        });
+    }
+
+    readDataTable = (db, table, callback) => {
+        implementationCodeIndexedDB(db, table, 'readonly', (objectStorage) => {
+            const requestCursor = objectStorage.openCursor();
+            const arrayData = new Array();
+            requestCursor.onsuccess = (e) => {
+                const cursor = requestCursor.result;
+                if (cursor) {
+                    arrayData.push(cursor.value);
+                    cursor.continue();
+                }
+            }
+            callback(arrayData);
+        });
+
+    }
+    #implementationCodeIndexedDB = (db, table, tipoTransaccion, callback) => {
+        let request = indexedDB.open(db, 1);
+
+        request.onsuccess = () => {
+            let DB = request.result;
+            const transaccion = DB.transaction([db], tipoTransaccion);
+            const objectStorage = transaccion.objectStore(table);
+            callback(objectStorage);
+        }
+    }
+
+
+
+
+
 }
-
-
-
